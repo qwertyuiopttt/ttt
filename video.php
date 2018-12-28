645dbb7511c130b24e85861adf0a6805a2bab408
qwertyuiopttt <2586375853@qq.com>
2018-12-28 09:09:11 +0800

    video

---
+++ video.php
@@ -0,0 +1,6 @@
+<meta charset='utf-8'>
+<form action='video2.php' method='post' enctype='multipart/form-data'>
+制作视频:<input type='file' name='video' size='40'><br>
+<input type='submit' value='上传'>&nbsp;&nbsp;
+<input type='reset' value='取消'>
+</form>
